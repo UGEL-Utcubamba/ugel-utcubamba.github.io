b513291c63782a6fb980ba16274503204aa292ff
GregoriohdD-tech <gbautista@ugelutcubamba.edu.pe>
2024-01-29 21:56:44 -0500

    lanzando prueba

---
+++ prueba/js/credenciales.js
@@ -0,0 +1,4 @@
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '196951549765-2nkohp29igtk5njjfn1bc3unk9iesgfq.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyBVC8PpgVrq85xt9UnbHParUi-O4pUBd8I';
+const SPREADSHEET = "1KJnpFXTXOHYfXbgNv8_9WYb7fcmgQMYcS2L-KANLYjI";
