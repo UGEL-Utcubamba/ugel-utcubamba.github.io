0eb5e38ac4f5e218f4f8d3991e5f4719a708a7be
GregoriohdD-tech <gbautista@ugelutcubamba.edu.pe>
2024-02-03 03:01:25 -0500

    actualizacion arepositorio

---
+++ prueba/js/repositorio.js
@@ -0,0 +1,22 @@
+const repositorio =[{
+  autor: "Gregorio",
+  Contenido: "Qué es STEM y STEAM",
+  enlace: "https://docs.google.com/presentation/d/1PACkZn8_vW-0U4woGm_kCFAMJQxLMcIk/edit?usp=sharing&ouid=105554935679150055636&rtpof=true&sd=true",
+  fecha: "",
+
+},
+{
+  autor: "Gregorio",
+  Contenido: "Qué es STEM y STEAM",
+  enlace: "https://docs.google.com/presentation/d/1PACkZn8_vW-0U4woGm_kCFAMJQxLMcIk/edit?usp=sharing&ouid=105554935679150055636&rtpof=true&sd=true",
+  fecha: "",
+},
+{
+  autor: "Gregorio",
+  Contenido: "Qué es STEM y STEAM",
+  enlace: "https://docs.google.com/presentation/d/1PACkZn8_vW-0U4woGm_kCFAMJQxLMcIk/edit?usp=sharing&ouid=105554935679150055636&rtpof=true&sd=true",
+  fecha: "",
+}
+]
+
+export default turnos;
