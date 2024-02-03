47b1d868eb38cb3ec0949b7b12457900dbaaef57
GregoriohdD-tech <gbautista@ugelutcubamba.edu.pe>
2024-02-03 09:44:07 -0500

    suamry

--- prueba/js/sheets.js
+++ prueba/js/sheets.js
@@ -1,7 +1,7 @@
 const hoja = "Repositorios";
-let repositorios;
+let turnos;
 
-async function getRepositorios() {
+async function getTurnos() {
   let response;
   try {
     response = await gapi.client.sheets.spreadsheets.values.get({
@@ -18,29 +18,29 @@ async function getRepositorios() {
     return;
   }
 
-  repositorios = [];
+  turnos = [];
   range.values.forEach((fila) => {
     if (isNaN(parseInt(fila[0])) || fila[5] !== undefined) return;
     const nuevoTurno = {
       id: fila[0],
-      autor: fila[1],
-      contenido: fila[2],
-      enlace: fila[3],
-      especialidad: fila[4],
-      fecha: fila[5],
+      cliente: fila[1],
+      email: fila[2],
+      modelo: fila[3],
+      problema: fila[4],
+      fecha_terminado: fila[5],
       comentario: fila[6]
     };
-    repositorios.push(nuevoTurno);
+    turnos.push(nuevoTurno);
   });
 }
 
 async function editTurno(id, contenido) {
   const update = [
     contenido.id,
-    contenido.autor,
-    contenido.contenido,
-    contenido.enlace,
-    contenido.especialidad,
+    contenido.cliente,
+    contenido.email,
+    contenido.modelo,
+    contenido.problema,
     new Date().toISOString(),
     contenido.comentario,
   ]
